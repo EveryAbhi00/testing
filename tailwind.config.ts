b6159410a6b89e54bdd19f26638400eaa0b0c689
EveryAbhi00 <abhimanu9616@gmail.com>
2025-05-04 20:48:24 +0530

    changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,8 @@ export default {
 		"./components/**/*.{ts,tsx}",
 		"./app/**/*.{ts,tsx}",
 		"./src/**/*.{ts,tsx}",
+		"./index.html", 
+		"./src/**/*.{js,ts,jsx,tsx}"
 	],
 	prefix: "",
 	theme: {
